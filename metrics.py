6bd57050ed071340826cfc2e6d95e31248b9a0ea
Administrator <manhngodh@gmail.com>
2020-09-09 15:00:52 +0700

    upload

---
+++ metrics.py
@@ -0,0 +1,12 @@
+from tensorflow.python import keras
+
+METRICS = [
+      keras.metrics.TruePositives(name='tp'),
+      keras.metrics.FalsePositives(name='fp'),
+      keras.metrics.TrueNegatives(name='tn'),
+      keras.metrics.FalseNegatives(name='fn'),
+      keras.metrics.BinaryAccuracy(name='accuracy'),
+      keras.metrics.Precision(name='precision'),
+      keras.metrics.Recall(name='recall'),
+      # keras.metrics.AUC(name='auc'),
+]
