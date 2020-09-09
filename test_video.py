d4d0575b90b9bf325b98128a1a4928c5ff43c663
Administrator <manhngodh@gmail.com>
2020-09-09 11:52:30 +0700

    upload

--- test_video.py
+++ test_video.py
@@ -40,10 +40,10 @@ if __name__ == '__main__':
             print(net_out_value, 'spoof')
             cv2.imwrite(f'tmp/{round(time.time()*1000)}.jpg', frame)
         # Display the resulting frame
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # cv2.imshow('frame', frame)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     break
 
     # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+    # cap.release()
+    # cv2.destroyAllWindows()
